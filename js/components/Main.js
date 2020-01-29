2eb6aa892fc767c0955f87b044d62a7a1d389a4e
kingdome4577 <Will@Taylors-MBP13.local>
2020-01-28 17:47:04 -0800

    functionality added to Main

--- js/components/Main.js
+++ js/components/Main.js
@@ -1,7 +1,15 @@
 // this will be our landing page we can use this to work with the MVP data we are trying to get
 import React from 'react';
-import { View, Text, Button, TouchableOpacity, StyleSheet } from 'react-native';
-import { Badge, Icon, withBadge } from 'react-native-elements';
+import {
+  View,
+  Text,
+  Button,
+  TouchableOpacity,
+  StyleSheet,
+  SafeAreaView,
+  ScrollView
+} from 'react-native';
+import { Badge } from 'react-native-elements';
 import { Dropdown } from 'react-native-material-dropdown';
 import { connect } from 'react-redux';
 import Login from './Login';
@@ -17,57 +25,57 @@ mapStateToProps = state => ({
 // where <Badge> is created we need to determine the error cases for clusters
 // so we can determine the status of the cluster and perhaps real time updates
 const Main = props => {
-  let namespaces = [
+  let regions = [
     {
-      value: 'default',
+      value: 'USA Central',
     },
     {
-      value: 'namespace1',
+      value: 'USA Pacific',
     },
     {
-      value: 'namespace2',
+      value: 'USA Eastern',
     },
     {
-      value: 'namespace3',
+      value: 'United Kingdom',
     },
     {
-      value: 'namespace4',
+      value: 'South Africa',
     },
     {
-      value: 'namespace5',
+      value: 'Madagascar',
     },
     {
-      value: 'namespace6',
+      value: 'Luxembourg',
     },
     {
-      value: 'namespace7',
+      value: 'Tattoine',
     },
   ];
 
-  let podList = [
-    'pod1',
-    'pod2',
-    'pod3',
-    'pod4',
-    'pod5',
-    'pod6',
-    'pod7',
-    'pod8',
-    'pod9',
-    'pod10',
-    'pod11',
-    'pod12',
-    'pod13',
-    'pod14',
-    'pod15',
-    'pod16',
+  let clusterList = [
+    'cluster1',
+    'cluster2',
+    'cluster3',
+    'cluster4',
+    'cluster5',
+    'cluster6',
+    'cluster7',
+    'cluster8',
+    'cluster9',
+    'cluster10',
+    'cluster11',
+    'cluster12',
+    'cluster13',
+    'cluster14',
+    'cluster15',
+    'cluster16',
   ];
-  const regions = [];
+  const clusterArr = [];
 
-  podList.forEach(pod => {
-    pods.push(
+  clusterList.forEach(cluster => {
+    clusterArr.push(
       <TouchableOpacity style={styles.podContainer} activeOpacity={0.7}>
-        <Text style={styles.podText}>{pod}</Text>
+        <Text style={styles.podText}>{clusterArr}</Text>
         <Text style={styles.statusText}>Status:</Text>
         <Badge status="success" badgeStyle={{ marginLeft: 13, marginTop: 6 }} />
       </TouchableOpacity>,
@@ -75,79 +83,109 @@ const Main = props => {
   });
 
   return (
-    let content = (
-      <View style={{ flex: 1, backgroundColor: 'orange', justifyContent: 'center' }}>
-        <SafeAreaView style={styles.safeArea}>
-          <ScrollView style={styles.scrollView}>
-            {/* <Text style={styles.test}>Select Namespace to View Pods</Text> */}
-            <Dropdown
-              label="Select a Namespace"
-              data={namespaces}
-              itemCount={3}
-              dropdownOffset={{ top: 15, left: 0 }}
-              style={styles.dropDown}
-            />
-            <ScrollView style={styles.podScroll}>{pods}</ScrollView>
-        </SafeAreaView>
-          <Dropdown>
-            Regions
-        </Dropdown>
+    <View>
+      <SafeAreaView style={styles.safeArea}>
+        <ScrollView style={styles.scrollView}>
+          {/* <Text style={styles.test}>Select Namespace to View Pods</Text> */}
+          <Dropdown
+            label="Select a Region"
+            data={regions}
+            itemCount={3}
+            dropdownOffset={{ top: 15, left: 0 }}
+            style={styles.dropDown}
+          />
+          <ScrollView style={styles.podScroll}>{}</ScrollView>
           <TouchableOpacity
-            style={styles.clusterButton}
-            activeOpacity={.7}
-            behavior="padding"
-            enabled
-            onPress={() => props.navigation.navigate("Pods")}
-          >
-            <Text> Cluster: {props.clusterName} Pods: {props.totalPods}  </Text>
-          </TouchableOpacity>
-          {/* <View>
-        <TouchableOpacity
-        style={styles.buttonsContainer}
-        behavior="padding"
-        enabled
-        onPress={() => props.navigation.navigate('Add')}>
-        <Text style={styles.buttonsText}> Add Cluster </Text>
-        </TouchableOpacity>
-      </View> */}
+            style={{
+              flex: 2,
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: 'blue',
+            }}
+            title=""
+            onPress={() => props.navigation.navigate('ShipM8')}
+          />
           <Button
             style={{
               flex: 2,
               justifyContent: 'center',
               alignItems: 'center',
-              backgroundColor: 'red',
+              backgroundColor: 'blue',
             }}
             title="Sign Out"
             onPress={() => props.navigation.navigate('ShipM8')}
           />
-        </View >
-        );
-        return content;
-      };
+        </ScrollView>
+      </SafeAreaView>
+    </View >
+  );
+};
 
-      export default connect(mapStateToProps)(React.memo(Main));
+export default connect(mapStateToProps)(React.memo(Main));
 
 const styles = StyleSheet.create({
-          clusterButton: {
-          backgroundColor: 'grey',
-        paddingVertical: 15,
-        borderRadius: 5,
-        marginTop: 10,
-        alignItems: 'center',
-        justifyContent: 'center'
-      },
+  clusterButton: {
+    backgroundColor: 'grey',
+    paddingVertical: 15,
+    borderRadius: 5,
+    marginTop: 10,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
   buttonsContainer: {
-          backgroundColor: 'blue',
-        paddingVertical: 15,
-        borderRadius: 5,
-        marginTop: 10,
-        alignItems: 'center',
-        justifyContent: 'center'
-      },
+    backgroundColor: 'blue',
+    paddingVertical: 15,
+    borderRadius: 5,
+    marginTop: 10,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
   buttonsText: {
-          textAlign: 'center',
-        color: 'white',
-        fontWeight: '700',
-        fontSize: 16,
-      },
-    });
+    textAlign: 'center',
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+  safeArea: {
+    backgroundColor: 'white',
+    marginHorizontal: 30,
+    height: '100%',
+  },
+  scrollView: {
+    marginHorizontal: 20,
+    marginTop: 30
+  },
+
+  namespacePickText: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  podContainer: {
+    marginTop: 10,
+    backgroundColor: 'gray',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginBottom: 6,
+    height: 40,
+    width: '100%',
+    paddingVertical: 10,
+    paddingLeft: 6,
+  },
+  podText: {
+    fontSize: 16,
+    marginRight: 22,
+    width: 200,
+    backgroundColor: 'white',
+  },
+  statusText: {
+    fontSize: 16,
+    backgroundColor: 'white',
+  },
+  podScroll: {
+    backgroundColor: 'pink',
+    marginTop: 10,
+    height: 580,
+  },
+});

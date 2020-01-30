b87ed1e3c4b82177fc638b61cccee3bc19acc24b
kingdome4577 <Will@Taylors-MBP13.local>
2020-01-29 19:37:44 -0800

    Updated Commit and Regions File/Dropdown

--- js/components/Main.js
+++ js/components/Main.js
@@ -13,7 +13,7 @@ import { Badge } from 'react-native-elements';
 import { Dropdown } from 'react-native-material-dropdown';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { connect } from 'react-redux';
-// import RegionsList from '../RegionsList'
+import Regions from '../Regions'
 
 mapStateToProps = state => ({
   totalCluster: state.app.totalCluster,
@@ -25,47 +25,6 @@ mapStateToProps = state => ({
 // where <Badge> is created we need to determine the error cases for clusters
 // so we can determine the status of the cluster and perhaps real time updates
 const Main = props => {
-  const regionsList = {
-    'US East (N. Virginia)': 'us-east-1',
-    'US East(Ohio)': 'us-east-2',
-    'US West(N.California)': 'us-west-1',
-    'US West(Oregon)': 'us-west-2',
-    'Asia Pacific(Hong Kong)': 'ap-east-1',
-    'Asia Pacific(Mumbai)': 'ap-south-1',
-    'Asia Pacific(Seoul)': 'ap-northeast-2',
-    'Asia Pacific(Singapore)': 'ap-southeast-1',
-    'Asia Pacific(Sydney)': 'ap-southeast-2',
-    'Asia Pacific(Tokyo)': 'ap-northeast-1',
-    'Canada(Central)': 'ca-central-1',
-    'Europe(Frankfurt)': 'eu-central-1',
-    'Europe(Ireland)': 'eu-west-1',
-    'Europe(London)': 'eu-west-2',
-    'Europe(Paris)': 'eu-west-3',
-    'Europe(Stockholm)': 'eu-north-1',
-    'Middle East(Bahrain)': 'me-south-1',
-    'South America(São Paulo)': 'sa-east-1'
-  }
-
-  const regionsListArr = [
-    { value: 'US East (N. Virginia)' },
-    { value: 'US East(Ohio)' },
-    { value: 'US West(N.California)' },
-    { value: 'US West(Oregon)' },
-    { value: 'Asia Pacific(Hong Kong)' },
-    { value: 'Asia Pacific(Mumbai)' },
-    { value: 'Asia Pacific(Seoul)' },
-    { value: 'Asia Pacific(Singapore)' },
-    { value: 'Asia Pacific(Sydney)' },
-    { value: 'Asia Pacific(Tokyo)' },
-    { value: 'Canada(Central)' },
-    { value: 'Europe(Frankfurt)' },
-    { value: 'Europe(Ireland)' },
-    { value: 'Europe(London)' },
-    { value: 'Europe(Paris)' },
-    { value: 'Europe(Stockholm)' },
-    { value: 'Middle East(Bahrain)' },
-    { value: 'South America(São Paulo)' }
-  ];
 
   let clusterList = [
     'cluster1',
@@ -117,22 +76,12 @@ const Main = props => {
           {/* <Text style={styles.test}>Select Namespace to View Pods</Text> */}
           <Dropdown
             label="Select a Region"
-            data={regionsListArr}
+            data={Regions}
             itemCount={4}
             dropdownOffset={{ top: 15, left: 0 }}
             style={styles.dropDown}
           />
           <ScrollView style={styles.clusterScroll}>{clusterArr}</ScrollView>
-          {/* <TouchableOpacity
-            style={styles.clusterButton}
-            behavior="padding"
-            enabled
-            onPress={() => props.navigation.navigate('Pods')}>
-            <Text>
-              {' '}
-              Cluster: {props.clusterName} Pods: {props.totalPods}{' '}
-            </Text>
-          </TouchableOpacity> */}
           <Button
             style={{
               flex: 2,

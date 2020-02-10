e8f70f8062442c2e1b3368d37642bc96f85d94dd
Taylor Rodrigues <taylor.d.rod33@gmail.com>
2020-02-09 18:27:39 -0800

    updated fake data

--- js/data/FakeData.js
+++ js/data/FakeData.js
@@ -1,13 +1,46 @@
 // AwsApi.fetchPodInfo(`https://56A5C5E4A41FCDF92D169B33AB0A5A5F.sk1.us-west-2.eks.amazonaws.com`, 'default', 'repark-deployment-7997f8b86d-4k5dd', 'awsTestCluster1')
 //   .then(res => console.log(res.spec.containers[0]))
 // gray #EFEFF4
-/**
- * fetchPodInfo:
- * {"apiVersion": "v1", "kind": "Pod", "metadata": {"annotations": {"kubernetes.io/psp": "eks.privileged"}, "creationTimestamp": "2020-01-29T00:27:40Z", "generateName": "repark-deployment-7997f8b86d-",
- * "labels": {"pod-template-hash": "7997f8b86d", "repark": "web"}, "name": "repark-deployment-7997f8b86d-4k5dd", "namespace": "default", "ownerReferences": [[Object]], "resourceVersion": "5456",
- * "selfLink": "/api/v1/namespaces/default/pods/repark-deployment-7997f8b86d-4k5dd", "uid": "287db3d7-422e-11ea-a037-02b853562b6a"}, "spec": {"containers": [[Object]], "dnsPolicy": "ClusterFirst",
- * "enableServiceLinks": true, "priority": 0, "restartPolicy": "Always", "schedulerName": "default-scheduler", "securityContext": {}, "serviceAccount": "default", "serviceAccountName": "default",
- * "terminationGracePeriodSeconds": 30, "tolerations": [[Object], [Object]], "volumes": [[Object]]}, "status": {"conditions": [[Object]], "phase": "Pending", "qosClass": "BestEffort"}} */
+/***/
+const fetchPods = {
+  apiVersion: "v1",
+  kind: "Pod",
+  metadata: {
+    annotations: {
+      "kubernetes.io/psp": "eks.privileged"
+    },
+    creationTimestamp: "2020-01-29T00:27:40Z",
+    generateName: "repark-deployment-7997f8b86d-",
+    labels: {
+      "pod-template-hash": "7997f8b86d", "repark": "web"
+    },
+    name: "repark-deployment-7997f8b86d-4k5dd",
+    namespace: "default",
+    ownerReferences: [[Object]],
+    resourceVersion: "5456",
+    selfLink: "/api/v1/namespaces/default/pods/repark-deployment-7997f8b86d-4k5dd",
+    uid: "287db3d7-422e-11ea-a037-02b853562b6a"
+  },
+  spec: {
+    containers: [[Object]],
+    dnsPolicy: "ClusterFirst",
+    enableServiceLinks: true,
+    priority: 0,
+    restartPolicy: "Always",
+    schedulerName: "default-scheduler",
+    securityContext: {},
+    serviceAccount: "default",
+    serviceAccountName: "default",
+    terminationGracePeriodSeconds: 30,
+    tolerations: [[Object], [Object]],
+    volumes: [[Object]]
+  },
+  status: {
+    conditions: [[Object]],
+    phase: "Pending",
+    qosClass: "BestEffort"
+  }
+}
 
 // metadata.ownerReferences: [{"apiVersion": "apps/v1", "blockOwnerDeletion": true, "controller": true, "kind": "ReplicaSet", "name": "repark-deployment-7997f8b86d", "uid": "287b9558-422e-11ea-a037-02b853562b6a"}]
 
@@ -24,5 +57,21 @@
 //                     "terminationMessagePath": "/dev/termination-log", "terminationMessagePolicy": "File", "volumeMounts": [{"mountPath": "/var/run/secrets/kubernetes.io/serviceaccount",
 //                     "name": "default-token-5rr84", "readOnly": true}]}
 
-//GoogleCloudApi.getProjects
-// { "projects": [{ "createTime": "2020-01-18T20:27:17.915Z", "lifecycleState": "ACTIVE", "name": "My Project 66285", "projectId": "primordial-saga-265520", "projectNumber": "535704856722" }, { "createTime": "2020-01-14T22:42:41.634Z", "lifecycleState": "ACTIVE", "name": "Learning Kubernetes", "projectId": "learning-kubernetes-265122", "projectNumber": "1048432399638" }] }
+const GoogleCloudApigetProjects = {
+  projects: [
+    {
+      createTime: "2020-01-18T20:27:17.915Z",
+      lifecycleState: "ACTIVE",
+      name: "My Project 66285",
+      projectId: "primordial-saga-265520",
+      projectNumber: "535704856722"
+    },
+    {
+      createTime: "2020-01-14T22:42:41.634Z",
+      lifecycleState: "ACTIVE",
+      name: "Learning Kubernetes",
+      projectId: "learning-kubernetes-265122",
+      projectNumber: "1048432399638"
+    }
+  ]
+}

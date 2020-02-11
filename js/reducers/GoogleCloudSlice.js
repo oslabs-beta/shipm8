13374a7ad98d892dfbb2cb01b5a5f8d46e81f8a3
Taylor Rodrigues <taylor.d.rod33@gmail.com>
2020-02-11 15:55:36 -0800

    projects, zones, clusters actions added to GoogleCloudSlice

--- js/reducers/GoogleCloudSlice.js
+++ js/reducers/GoogleCloudSlice.js
@@ -1,15 +1,48 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+import { startLoading, loadingFailed } from '../utils/LoadingUtils';
+
 const GoogleCloud = createSlice({
   name: 'GoogleCloud',
   initialState: {
-
+    isLoading: false,
+    error: null,
+    zones: null,
+    projects: null,
+    clusters: null,
   },
   reducers: {
-
+    fetchProjectsStart: startLoading,
+    fetchProjectsFailed: loadingFailed,
+    fetchProjectsSuccess(state, action) {
+      const projects = action.payload;
+      state.projects = projects;
+    },
+    fetchZonesStart: startLoading,
+    fetchZonesFailed: loadingFailed,
+    fetchZonesSuccess(state, action) {
+      const zones = action.payload;
+      state.zones = zones;
+    },
+    fetchGkeClustersStart: startLoading,
+    fetchGkeClustersFailed: loadingFailed,
+    fetchGkeClustersSuccess(state, action) {
+      const clusters = action.payload;
+      state.clusters = clusters;
+    }
   }
 });
 
-export const { } = GoogleCloud.actions;
+export const {
+  fetchProjectsStart,
+  fetchProjectsFailed,
+  fetchProjectsSuccess,
+  fetchZonesStart,
+  fetchZonesFailed,
+  fetchZonesSuccess,
+  fetchGkeClustersStart,
+  fetchGkeClustersFailed,
+  fetchGkeClustersSuccess,
+} = GoogleCloud.actions;
 
 export default GoogleCloud.reducer;

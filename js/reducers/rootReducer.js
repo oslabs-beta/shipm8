2f23fecc33807c04ffb43945f2654cba3855a4be
Taylor Rodrigues <taylor.d.rod33@gmail.com>
2020-02-11 14:46:26 -0800

    updated rootReducer to add AwsSlice

--- js/reducers/rootReducer.js
+++ js/reducers/rootReducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from '@reduxjs/toolkit';
 
 import clustersReducer from '../components/Clusters/ClustersSlice';
 import podsReducer from '../components/Pods/PodsSlice';
+import AwsReducer from '../components/AwsSlice';
 
 const rootReducer = combineReducers({
   Clusters: clustersReducer,
   Pods: podsReducer,
+  Aws: AwsReducer,
 });
 
 export default rootReducer;

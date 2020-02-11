5c1e55d125c105a9d7109a043d284f665ff31379
Taylor Rodrigues <taylor.d.rod33@gmail.com>
2020-02-11 15:49:04 -0800

    GoogleCloudSlice base started

---
+++ js/reducers/GoogleCloudSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const GoogleCloud = createSlice({
+  name: 'GoogleCloud',
+  initialState: {
+
+  },
+  reducers: {
+
+  }
+});
+
+export const { } = GoogleCloud.actions;
+
+export default GoogleCloud.reducer;

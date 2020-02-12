56d2049a16f2d807ab6b03df6b56aad10a024977
Taylor Rodrigues <taylor.d.rod33@gmail.com>
2020-02-12 12:23:03 -0800

    updated GoogleCloud actions/thunks to include Gcp in name & added changing isLoading to false for all success actions

--- js/reducers/GoogleCloudSlice.js
+++ js/reducers/GoogleCloudSlice.js
@@ -14,45 +14,50 @@ const GoogleCloud = createSlice({
     clusters: null,
   },
   reducers: {
-    fetchProjectsStart: startLoading,
-    fetchProjectsFailed: loadingFailed,
-    fetchProjectsSuccess(state, action) {
+    fetchGcpProjectsStart: startLoading,
+    fetchGcpProjectsFailed: loadingFailed,
+    fetchGcpProjectsSuccess(state, action) {
       const projects = action.payload;
       state.projects = projects;
+      state.isLoading = false;
     },
-    fetchZonesStart: startLoading,
-    fetchZonesFailed: loadingFailed,
-    fetchZonesSuccess(state, action) {
+    fetchGcpZonesStart: startLoading,
+    fetchGcpZonesFailed: loadingFailed,
+    fetchGcpZonesSuccess(state, action) {
       const zones = action.payload;
       state.zones = zones;
+      state.isLoading = false;
     },
     fetchGkeClustersStart: startLoading,
     fetchGkeClustersFailed: loadingFailed,
     fetchGkeClustersSuccess(state, action) {
       const clusters = action.payload;
       state.clusters = clusters;
+      state.isLoading = false;
     },
     googleSignInStart: startLoading,
     googleSignInFailed: loadingFailed,
     googleSignInSuccess(state, action) {
       const user = action.payload;
       state.user = user;
+      state.isLoading = false;
     },
     googleSignOutStart: startLoading,
     googleSignOutFailed: loadingFailed,
     googleSignOutSuccess(state, action) {
       state.user = null;
+      state.isLoading = false;
     }
   }
 });
 
 export const {
-  fetchProjectsStart,
-  fetchProjectsFailed,
-  fetchProjectsSuccess,
-  fetchZonesStart,
-  fetchZonesFailed,
-  fetchZonesSuccess,
+  fetchGcpProjectsStart,
+  fetchGcpProjectsFailed,
+  fetchGcpProjectsSuccess,
+  fetchGcpZonesStart,
+  fetchGcpZonesFailed,
+  fetchGcpZonesSuccess,
   fetchGkeClustersStart,
   fetchGkeClustersFailed,
   fetchGkeClustersSuccess,
@@ -73,13 +78,14 @@ export const googleSignIn = () =>
       dispatch(googleSignInStart());
       const user = await GoogleCloudApi.signIn();
       dispatch(googleSignInSuccess(user));
+      return Promise.resolve(true);
     } catch (err) {
       dispatch(googleSignInFailed(err.toString()));
       return Promise.reject(err);
     }
   }
 
-export const googleSignOut = () => {
+export const googleSignOut = () =>
   async dispatch => {
     try {
       dispatch(googleSignOutStart());
@@ -91,48 +97,48 @@ export const googleSignOut = () => {
     }
   }
 
-  export const fetchGkeClusters = (projectId, zone) =>
-    async dispatch => {
-      try {
-        dispatch(fetchGkeClustersStart());
-        const clusters = await GoogleCloudApi.fetchGkeClusters(projectId, zone);
-        dispatch(fetchGkeClustersSuccess(clusters));
-      } catch (err) {
-        dispatch(fetchGkeClustersFailed(err.toString()));
-        return Promise.reject(err);
-      }
+export const fetchGkeClusters = (projectId, zone) =>
+  async dispatch => {
+    try {
+      dispatch(fetchGkeClustersStart());
+      const clusters = await GoogleCloudApi.fetchGkeClusters(projectId, zone);
+      dispatch(fetchGkeClustersSuccess(clusters));
+    } catch (err) {
+      dispatch(fetchGkeClustersFailed(err.toString()));
+      return Promise.reject(err);
     }
+  }
 
-  export const fetchGcpProjects = pageToken =>
-    async dispatch => {
-      try {
-        dispatch(fetchProjectsStart());
-        const projects = await GoogleCloudApi.fetchProjects(pageToken);
-        if (projects.error) {
-          dispatch(fetchProjectsFailed(projects.error.toString()));
-          return Promise.reject(projects.error.message);
-        }
-        dispatch(fetchProjectsSuccess(projects.projects));
-        if (projects.nextPageToken) { return dispatch(fetchProjects(projects.nextPageToken)); }
-        return Promise.resolve('success');
-      } catch (err) {
-        dispatch(fetchGkeClustersFailed(err.toString()));
-        return alert(err);
+export const fetchGcpProjects = pageToken =>
+  async dispatch => {
+    try {
+      dispatch(fetchGcpProjectsStart());
+      const projects = await GoogleCloudApi.fetchProjects(pageToken);
+      if (projects.error) {
+        dispatch(fetchGcpProjectsFailed(projects.error.toString()));
+        return Promise.reject(projects.error.message);
       }
+      dispatch(fetchGcpProjectsSuccess(projects.projects));
+      if (projects.nextPageToken) { return dispatch(fetchGcpProjects(projects.nextPageToken)); }
+      return Promise.resolve();
+    } catch (err) {
+      dispatch(fetchGcpProjectsFailed(err.toString()));
+      return alert(err);
     }
+  }
 
-  export const fetchGcpZones = (projectId, pageToken) =>
-    async dispatch => {
-      try {
-        dispatch(fetchZonesStart());
-        const zones = await GoogleCloudApi.fetchZones(projectId, pageToken);
-        dispatch(fetchZonesSuccess(zones.zones));
-        if (zones.nextPageToken) {
-          return dispatch(fetchZones(projectId, zones.nextPageToken));
-        }
-        return Promise.resolve();
-      } catch (err) {
-        dispatch(fetchZonesFailed(err.toString()));
-        return Promise.reject(err)
+export const fetchGcpZones = (projectId, pageToken) =>
+  async dispatch => {
+    try {
+      dispatch(fetchGcpZonesStart());
+      const zones = await GoogleCloudApi.fetchZones(projectId, pageToken);
+      dispatch(fetchGcpZonesSuccess(zones.zones));
+      if (zones.nextPageToken) {
+        return dispatch(fetchGcpZones(projectId, zones.nextPageToken));
       }
+      return Promise.resolve();
+    } catch (err) {
+      dispatch(fetchGcpZonesFailed(err.toString()));
+      return Promise.reject(err)
     }
+  }

2c5c3d11dc4319e3dc8e3341a9b2ee1930246879
Taylor Rodrigues <taylor.d.rod33@gmail.com>
2020-02-15 14:41:32 -0800

    disable yellowbox

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ import { AppRegistry } from 'react-native';
 import AppContainer from './js/Navigation';
 import { name as appName } from './app.json';
 import EStyleSheet from 'react-native-extended-stylesheet';
+import { YellowBox } from 'react-native';
 
+YellowBox.ignoreWarnings(['Warning:']);
 AppRegistry.registerComponent(appName, () => AppContainer);
 
 EStyleSheet.build();

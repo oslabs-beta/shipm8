bece78334fbdf6231e22b3d037ccebd377c5bb92
Taylor Rodrigues <taylor.d.rod33@gmail.com>
2020-02-24 12:28:48 -0800

    adjust cloudLogin

--- js/components/CloudLogin.js
+++ js/components/CloudLogin.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { View, KeyboardAvoidingView } from 'react-native';
+import { KeyboardAvoidingView } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
 import Login from './Login';
 
 const CloudLogin = ({ navigation }) => {
   return (
-    <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
-      <View>
-        <Login navigation={navigation} />
-      </View>
+    <KeyboardAvoidingView
+      // contentContainerStyle={styles.container}
+      style={styles.container}
+      behavior="position">
+      <Login navigation={navigation} />
     </KeyboardAvoidingView>
   );
 };
@@ -19,12 +20,8 @@ export default React.memo(CloudLogin);
 const styles = EStyleSheet.create({
   container: {
     flex: 1,
+    padding: '2rem',
     backgroundColor: 'white',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  logoContainer: {
-    flexGrow: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },

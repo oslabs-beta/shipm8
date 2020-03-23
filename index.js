592785a48904765ded4bf88891b952fe23ec4ae9
Taylor Rodrigues <taylor.d.rod33@gmail.com>
2020-03-22 18:58:58 -0700

    change App location

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import { AppRegistry } from 'react-native';
-import App from './src/Navigation';
+import App from './App';
 import { name as appName } from './app.json';
 
 console.disableYellowBox = true;

17fc70a40eb17d57cc05b671265ccbdda9164913
Taylor Rodrigues <taylor.d.rod33@gmail.com>
2020-03-22 17:53:13 -0700

    renamed js to src

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import { AppRegistry } from 'react-native';
-import AppContainer from './js/Navigation';
+import AppContainer from './src/Navigation';
 import { name as appName } from './app.json';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import { YellowBox } from 'react-native';
